57e17875350eac39443544436cd096c779db79d0
Leo <82315240+leohonlmc@users.noreply.github.com>
2023-12-13 10:56:28 -0500

    stocks bookmark fetched

--- src/components/Bookmark.jsx
+++ src/components/Bookmark.jsx
@@ -3,7 +3,7 @@ import React, { useRef, useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Bookmark(props) {
-  console.log(props.stocks);
+  // console.log(props.stocks);
 
   return (
     <div className="Bookmark">
@@ -17,12 +17,31 @@ function Bookmark(props) {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="ticker">No data</td>
-            <td className="latest">No data</td>
-            <td className="change">No data</td>
-            <td className="percent">No data</td>
-          </tr>
+          {props.stocks.map((dataItem, index) => {
+            const timeSeries = dataItem["Time Series (5min)"];
+            const sortedTimestamps = Object.keys(timeSeries).sort();
+            const earliestTimestamp = sortedTimestamps[0];
+            const latestTimestamp =
+              sortedTimestamps[sortedTimestamps.length - 1];
+
+            const earliestData = timeSeries[earliestTimestamp];
+            const latestData = timeSeries[latestTimestamp];
+
+            const openPrice = parseFloat(earliestData["1. open"]);
+            const closePrice = parseFloat(latestData["4. close"]);
+
+            const change = closePrice - openPrice;
+            const percentChange = (change / openPrice) * 100;
+
+            return (
+              <tr key={index}>
+                <td className="ticker">{dataItem["Meta Data"]["2. Symbol"]}</td>
+                <td className="latest">{closePrice.toFixed(2)}</td>
+                <td className="change">{change.toFixed(2)}</td>
+                <td className="percent">{percentChange.toFixed(2)}%</td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
